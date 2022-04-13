a9a9734e6819e0a20072e1044d0addccc1f6bf05
Gonzalo Quiroz <kiyegoo@gmail.com>
2022-04-12 19:49:35 -0500

    Change connection to mongo

--- app.js
+++ app.js
@@ -11,19 +11,16 @@ const path = require("path");
 const passport = require("./helpers/passport");
 const session = require("express-session");
 
-mongoose
-  .connect(
-    process.env.DB,
-    { useNewUrlParser: true }
-  )
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
+const connectToMongo = async () => {
+  await mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
   });
+  return mongoose;
+};
+
+connectToMongo().then(async (x) => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+});
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
@@ -36,7 +33,7 @@ app.use(
   session({
     secret: process.env.SECRET,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -55,7 +52,7 @@ app.use(
   require("node-sass-middleware")({
     src: path.join(__dirname, "public"),
     dest: path.join(__dirname, "public"),
-    sourceMap: true
+    sourceMap: true,
   })
 );
 
